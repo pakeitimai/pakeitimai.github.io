833bf091e053c1c8c407d9c03832db625ace8658
Marius Gegoevskis <m.gzegozevskis@eif.viko.lt>
2018-09-11 22:50:31 +0300

    saerw

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -31,3 +31,23 @@ self.addEventListener('refreshOffline', function(response) {
     return cache.put(offlinePage, response);
   });
 });
+
+self.addEventListener('notificationclick', function(event) {
+    // Close notification.
+    event.notification.close();
+
+    // Example: Open window after 3 seconds.
+    // (doing so is a terrible user experience by the way, because
+    //  the user is left wondering what happens for 3 seconds.)
+    var promise = new Promise(function(resolve) {
+        setTimeout(resolve, 1000);
+    }).then(function() {
+        // return the promise returned by openWindow, just in case.
+        // Opening any origin only works in Chrome 43+.
+        return clients.openWindow('https://pakeitimai.github.io');
+    });
+
+    // Now wait for the promise to keep the permission alive.
+    event.waitUntil(promise);
+});
+
