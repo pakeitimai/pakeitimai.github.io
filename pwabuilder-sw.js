4265c67103574e6d7a3f91f21df5b518e34d6e4a
Marius Gegoevskis <m.gzegozevskis@eif.viko.lt>
2018-09-12 19:55:15 +0300

    saerw

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -40,7 +40,7 @@ self.addEventListener('notificationclick', function(event) {
     // (doing so is a terrible user experience by the way, because
     //  the user is left wondering what happens for 3 seconds.)
     var promise = new Promise(function(resolve) {
-        setTimeout(resolve, 1000);
+        setTimeout(resolve, 100);
     }).then(function() {
         // return the promise returned by openWindow, just in case.
         // Opening any origin only works in Chrome 43+.
